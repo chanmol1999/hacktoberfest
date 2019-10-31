1df42d673103cea0ae7de26fc941d76d9f22d6bd
Anik Datta <anikdatta05@gmail.com>
2019-10-31 13:00:05 +0530

    MAD LIBS GAME

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+name=input("Enter any name: ")
+prof=input("Enter unrealistic profession: ")
+country=input("Enter country: ")
+nam=input("Enter another name: ")
+colour=input("Enter colour: ")
+adjective=input("Enter any adjective: ")
+
+
+print("\n Meet our hero "+name+", a super intelligent "+prof+".\n A run in with the "+country+" military leads him to create his alter-ego "+nam+",\n a "+colour+" "+adjective+" giant capable of great destruction.")
